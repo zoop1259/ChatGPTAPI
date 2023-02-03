7241e2b21ed4b883c93d53908cbefa98e1cae0c8
zoop1259 <kdm43211991@gmail.com>
2023-02-03 19:14:37 +0900

    Initial Commit

---
+++ ChatGPTAPI/ChatGPTAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAPIApp.swift
+//  ChatGPTAPI
+//
+//  Created by 강대민 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
